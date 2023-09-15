5659a89a94ed43ed26fa3a3f5397d1834415890a
ArthurRufi <temparthur87@gmail.com>
2023-09-15 10:29:13 -0300

    teste update 1.3

---
+++ src/TiposDeVariaveis.java
@@ -0,0 +1,10 @@
+public class TiposDeVariaveis {
+    public static void main(String[] args) throws Exception {
+        double salariominimo =  2500;
+
+        short numeroCurto = 1; 
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+    }
+}
